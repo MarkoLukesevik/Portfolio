48cb4ab4e6c5f6b1483b990c997f40d601ea3efb
Marko Lukesevik <marko_lukesevik@protonmail.com>
2022-06-02 18:58:33 +0200

    adding new project

--- src/components/Utils/PortfolioData.js
+++ src/components/Utils/PortfolioData.js
@@ -1,5 +1,21 @@
 export const portfolioData = [
   {
+    id: 4,
+    img: "audiophile.jpg",
+    title: "Audiophile",
+    text: "This React.js e-commerce project required me to build a fully responsive website, also add/remove products from the cart, edit product quantities, receive form validations and a lot more.I was working with JSON data, axios, routing, state management, Context and localStorage.",
+    title2: "Project Background",
+    textBg:
+      "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    previewTitle: "Static Previews",
+    previewimg1: "audiophileInner.jpg",
+    previewimg2: "audiophileTablet.jpg",
+    skills1: "Interactive Design / Front End Development",
+    skills2: "HTML / CSS / JS / React.js",
+    link: "https://audiophile-e-commerce-markolukesevik.vercel.app/",
+  },
+
+  {
     id: 3,
     img: "entertainment.jpg",
     title: "MoviesApp",
