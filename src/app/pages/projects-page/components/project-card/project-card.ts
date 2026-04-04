4beb9bb33ce92f30fa07efa0077b40b6c175ac65
Marko <marko.lukesevik@protonmail.com>
2026-04-04 22:35:55 +0200

    sliding touch

--- src/app/pages/projects-page/components/project-card/project-card.ts
+++ src/app/pages/projects-page/components/project-card/project-card.ts
@@ -21,6 +21,9 @@ export class ProjectCard {
   @Input({ required: true }) project!: Project;
 
   currentImageIndex = 0;
+  private startX = 0;
+  private currentX = 0;
+  private isDragging = false;
 
   nextImage() {
     if (!this.project?.images?.length) return;
@@ -31,5 +34,32 @@ export class ProjectCard {
     if (!this.project?.images?.length) return;
     this.currentImageIndex =
       (this.currentImageIndex - 1 + this.project.images.length) % this.project.images.length;
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+    this.isDragging = true;
+  }
+
+  onTouchMove(event: TouchEvent) {
+    if (!this.isDragging) return;
+    this.currentX = event.touches[0].clientX;
+  }
+
+  onTouchEnd() {
+    if (!this.isDragging) return;
+
+    const diff = this.startX - this.currentX;
+
+    // sensitivity threshold (important!)
+    const threshold = 50;
+
+    if (diff > threshold) {
+      this.nextImage(); // swipe left
+    } else if (diff < -threshold) {
+      this.prevImage(); // swipe right
+    }
+
+    this.isDragging = false;
   }
 }
